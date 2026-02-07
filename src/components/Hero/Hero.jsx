a7a7eb868808614e9ec93ddc75010561545c386b
Prince Singh <shiprincesingh@gmail.com>
2026-02-07 17:54:16 +0530

    error solve

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Grocery from "../../src/assets/grocery.png";
+import Grocery from "../../assets/grocery.png";
 import Button from "../Button/Button";
 
 const Hero = () => {
